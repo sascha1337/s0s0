570f6ad8aaace4c4c905b9896c9833ac23477249
Serkan Reis <serkanreis@gmail.com>
2022-04-15 17:10:59 +0300

    TailwindCSS & CosmJS dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './components/**/*.tsx',
+    './contexts/**/*.tsx',
+    './pages/**/*.tsx',
+    './utils/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
